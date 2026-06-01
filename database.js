52eaee4758ecea4f3f4a3ad43e22184b87ff0ce7
pozole <bryam846@gmail.com>
2026-05-31 18:50:36 -0700

    actualizado

--- database.js
+++ database.js
@@ -1,28 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-let sequelize;
-
-
-if (process.env.DATABASE_URL) {
-    // Conexion para Render (PostgreSQL)
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        },
-        logging: false
-    });
-} else {
-    // Conexion local para desarrollo (SQLite)
-    sequelize = new Sequelize({
-        dialect: 'sqlite',
-        storage: 'database.sqlite',
-        logging: false
-    });
-}
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false
+});
 
 module.exports = sequelize;
